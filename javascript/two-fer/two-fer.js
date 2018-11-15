022c4b1db7d518924ea7da09e9e9ee3ca2558e27
Charles Clemens <cclemensjr@gmail.com>
2018-11-14 19:54:19 -0800

    Refactored twoFer into a oneliner

--- javascript/two-fer/two-fer.js
+++ javascript/two-fer/two-fer.js
@@ -1,3 +1,1 @@
-export const twoFer = (name) => {
-  return (name === '') ? 'One for you, one for me.' : `One for ${name}, one for me.`;
-};
+export const twoFer = name => ((name === '') ? 'One for you, one for me.' : `One for ${name}, one for me.`);
