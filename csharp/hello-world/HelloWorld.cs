04018d66746cccb91254a65c3fbf721b80beb4dd
Charles Clemens <cclemensjr@gmail.com>
2018-11-14 16:47:09 -0800

    Initial Commit

---
+++ csharp/hello-world/HelloWorld.cs
@@ -0,0 +1,10 @@
+using System;
+
+public static class HelloWorld
+{
+    public static string Hello()
+    {
+        //throw new NotImplementedException("You need to implement this function.");
+        return ("Hello, World!");
+    }
+}
