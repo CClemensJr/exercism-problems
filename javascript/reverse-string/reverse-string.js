9b8938d18a02ea8cef888bc4f9b1c7bd963d57d4
Charles Clemens <cclemensjr@gmail.com>
2018-11-14 21:20:06 -0800

    Fleshed out reverseString function

--- javascript/reverse-string/reverse-string.js
+++ javascript/reverse-string/reverse-string.js
@@ -1,3 +1,10 @@
-export const  reverseString = string => {
-  
+export const reverseString = (str) => {
+  const strArr = str.split('');
+  const rts = [];
+
+  strArr.forEach((char) => {
+    rts.push(char);
+  });
+
+  return rts.join('');
 };
