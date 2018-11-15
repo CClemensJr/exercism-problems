6d926fe7d36278add73dc0f08b562b0a3f5944ea
Charles Clemens <cclemensjr@gmail.com>
2018-11-14 21:57:28 -0800

    Updated dotnet, passed all tests

--- csharp/leap/LeapTest.cs
+++ csharp/leap/LeapTest.cs
@@ -10,25 +10,25 @@ public class LeapTest
         Assert.False(Leap.IsLeapYear(2015));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Year_divisible_by_4_not_divisible_by_100_is_leap_year()
     {
         Assert.True(Leap.IsLeapYear(1996));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Year_divisible_by_100_not_divisible_by_400_is_common_year()
     {
         Assert.False(Leap.IsLeapYear(2100));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Year_divisible_by_400_is_leap_year()
     {
         Assert.True(Leap.IsLeapYear(2000));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Year_divisible_by_200_not_divisible_by_400_is_common_year()
     {
         Assert.False(Leap.IsLeapYear(1800));
