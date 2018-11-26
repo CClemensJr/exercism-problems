36e65d4be972ff02ee923fcd3327bdc06405c592
Charles Clemens <cclemensjr@gmail.com>
2018-11-26 11:28:46 -0800

    Passed test 5

--- csharp/bob/BobTest.cs
+++ csharp/bob/BobTest.cs
@@ -28,7 +28,7 @@ public class BobTest
         Assert.Equal("Sure.", Bob.Response("Does this cryogenic chamber make me look fat?"));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Asking_a_numeric_question()
     {
         Assert.Equal("Sure.", Bob.Response("You are, what, like 15?"));
