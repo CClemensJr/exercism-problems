18a2eb77436a98424a743304a22a2e713d9a6d7b
Charles Clemens <cclemensjr@gmail.com>
2018-11-25 20:09:18 -0800

    Passed first test

--- csharp/bob/Bob.cs
+++ csharp/bob/Bob.cs
@@ -4,6 +4,8 @@ public static class Bob
 {
     public static string Response(string statement)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        string[] responses = {"Sure.", "Whoa, chill out!", "Calm down, I know what I'm doing!", "Fine. Be that way!", "Whatever."};
+
+        return responses[4];
     }
 }
